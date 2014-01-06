a885bc4025d2901e9199d267485017a91a4631cf
Aji Achmad Mustofa <micro280991@gmail.com>
2014-01-06 08:07:47 +0700

    Fix List of TargetSaving and ReCreate add_target_saving

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,8 +6,9 @@ class DashboardController < ApplicationController
     @event  = Event.new
     @target_saving = TargetSaving.new
     @target_savings = current_user.target_savings
-    @total_income   = current_user.events.sum('income')
-    @total_outcome  = current_user.events.sum('outcome')
+    @total_income_all   = current_user.events.sum('income')
+    @total_outcome_all  = current_user.events.sum('outcome')
+    @total = @total_income_all - @total_outcome_all
     # respond_to do |format|
     #   format.js { @tes = params['name'] }
     # end
--- app/controllers/savings_controller.rb
+++ app/controllers/savings_controller.rb
@@ -3,6 +3,10 @@ class SavingsController < ApplicationController
 
   def index
     @target_savings = current_user.target_savings
+    @target_saving = TargetSaving.new
+    @total_income_all   = current_user.events.sum('income')
+    @total_outcome_all  = current_user.events.sum('outcome')
+    @total = @total_income_all - @total_outcome_all
   end
   def create
     @target_saving  = TargetSaving.add_target params, current_user 
