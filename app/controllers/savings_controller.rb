8d46d3210ef0fa1b3de168c6f076e196effb0c3f
Domikado 3 <domikado3@Domikado-3.local>
2014-01-06 13:11:37 +0700

    save target saving

--- app/controllers/savings_controller.rb
+++ app/controllers/savings_controller.rb
@@ -1,5 +1,6 @@
 class SavingsController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user! 
+  before_action :set_target_savings, only: [:show, :edit, :update, :destroy]
 
   def index
     @target_savings = current_user.target_savings
@@ -15,8 +16,36 @@ class SavingsController < ApplicationController
     @total_expenses = current_user.events.by_month(m,year:y).sum('outcome')
     @total_month = @total_income - @total_expenses
   end
+
+  def new
+    @target_saving = TargetSaving.new
+  end
+
   def create
     @target_saving  = TargetSaving.add_target params, current_user 
     #debugger
+    respond_to do |format|
+      if @target_saving.save
+        format.html { redirect_to savings_index_path, notice: 'Target Savings was successfully created.' }
+      else
+        format.html { render action: 'new' }
+      end
+    end
   end
+
+  # def destroy
+  #   @target_saving.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to target_saving_url }
+  #   end
+  # end
+
+  # private
+  #   def set_target_savings
+  #     @target_saving = TargetSaving.find(params[:id])
+  #   end
+
+    # def target_savings_params
+    #   params.require(:target_savings).permit(:name, :description, :url, :amount_target, :price, :deadline, :user_id, :images)
+    # end
 end
