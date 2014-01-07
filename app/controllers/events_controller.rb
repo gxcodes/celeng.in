e2526e4ce85ade00d38db7e2b728794af35bdb1f
gxcodes <gxpratama@gmail.com>
2014-01-07 13:57:53 +0700

    fix notifications word and style

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,18 +9,18 @@ class EventsController < ApplicationController
     respond_to do |format|
       if params[:transaksi] == "add_outcome" || params[:transaksi] == "add_income"
         if @event.save
-          format.html { redirect_to dashboard_index_path, notice: 'successfully' }
+          format.html { redirect_to dashboard_index_path, notice: 'Transaction added successfully!' }
         else
           format.html { render action: "new" }
         end
       elsif Event.check_balance(@event)
         if @event.save
-          format.html { redirect_to dashboard_index_path, notice: 'successfully' }
+          format.html { redirect_to dashboard_index_path, notice: 'Savings added successfully!' }
         else
           format.html { render action: "new" }
         end
       else
-        format.html { redirect_to dashboard_index_path, notice: 'Overload' }
+        format.html { redirect_to dashboard_index_path, alert: 'Amount error. You can not add more than the amount left to reach the target savings.' }
       end
     end
   end
