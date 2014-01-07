e5e79a9d981984fa6b5633ea546eb4e8733f1d14
Aji Achmad Mustofa <micro280991@gmail.com>
2014-01-07 14:28:39 +0700

    Add New Route Controller and Link Detail in SideBar panel

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Celengin::Application.routes.draw do
   root 'home#index'
   devise_for :users, :path => '', :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'signup' }
   get   "dashboard/index"
-  resources :examples#, :events
+  resources :target_savings, only: [:index] do #, :events
+    resources :events, only: [:index] 
+  end
+  # resources :posts do
+  #   resources :comments, only: [:index, :new, :create]
+  # end
   post  "events" => "events#create"
   post  "target_savings" => "savings#create"
 end
