26c5f80367ca30e2280e98b7c1ee37554a41063f
gxcodes <gxpratama@gmail.com>
2014-01-08 16:00:12 +0700

    add footer and small fix here and there

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -45,4 +45,18 @@ $( document ).ready( function(){
       $( "#select_saving" ).hide('fast');
     }
   });
+
+  $(window).bind("load", function () {
+      var footer = $("#footer");
+      var pos = footer.position();
+      var height = $(window).height();
+      height = height - pos.top;
+      height = height - footer.height();
+      if (height > 0) {
+          footer.css({
+              'margin-top': height + 'px'
+          });
+      }
+  });
+  
 });
