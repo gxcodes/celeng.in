9cc893e48e1e8115613b2ec1878528d72d45f377
Wawan Kurniawan <ones07@gmail.com>
2014-01-10 15:20:27 +0700

    capistrano & unicorn

---
+++ config/unicorn/production.rb
@@ -0,0 +1,34 @@
+rails_env = ENV['RAILS_ENV'] || 'production'
+
+working_directory "/home/akademirails/celeng.in/current"
+pid "/home/akademirails/celeng.in/shared/tmp/pids/unicorn.pid"
+stderr_path "/home/akademirails/celeng.in/shared/log/unicorn.log"
+stdout_path "/home/akademirails/celeng.in/shared/log/unicorn.log"
+
+listen "/tmp/unicorn.celeng.in.sock"
+worker_processes 2
+timeout 90
+
+preload_app true
+
+before_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+
+end
